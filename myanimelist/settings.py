4048e4a827d37aab5c423680e90da36cb2df7f34
Ruby-blues <tsholofelo.mphaga2006@gmail.com>
2024-05-07 13:30:55 +0200

    update static images pt3

--- myanimelist/settings.py
+++ myanimelist/settings.py
@@ -133,7 +133,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [
     BASE_DIR/'animelist/static/',
-    BASE_DIR/'static/images',
 ]
 django_heroku.settings(locals())
 
